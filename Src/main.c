da3ad965e58b35561c8cec3dc9d05d37b1b2b2a0
Gabriel Edilson de Miranda <gabrielmirandadev32005@gmail.com>
2026-08-18 14:11:51 -0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -21,6 +21,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -57,13 +58,23 @@ BME280_Data_t BME280;
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
 /* USER CODE BEGIN PV */
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_USART2_UART_Init(void);
+static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_USART2_UART_Init(void);
+void StartDefaultTask(void *argument);
 void Sensor_Init(void);
 
 /* USER CODE BEGIN PFP */
@@ -111,6 +122,7 @@ int main(void)
   BNO080_Initialization();
   BNO080_enableRotationVector(2500); //enable rotation vector at 400Hz
   BNO080_enableAccelerometer(2500);
+  BNO080_enableMagnetometer(2500);
   BNO080_calibrateAll();
   HAL_Delay(200);
   Sensor_Init();
@@ -120,37 +132,35 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
-  { /* USER CODE END WHILE */
+  {
 
-	    if (!BNO080_dataAvailable())
-	    {
-	      q[0] = BNO080_getQuatI();
-	      q[1] = BNO080_getQuatJ();
-	      q[2] = BNO080_getQuatK();
-	      q[3] = BNO080_getQuatReal();
-	      accel[0] = BNO080_getAccelX();
-	      accel[1] = BNO080_getAccelY();
-	      accel[2] = BNO080_getAccelZ();
-	      BME280Calculation(&BME280);
-	      printf("Temperatura%f\n", BME280.Temperature);
-	      printf("Pressão%f\n", BME280.Pressure);
-	      printf("Altitude%f\n", BME280.AltitudeTP);
-	      printf("%f\n", BME280.Humidity);
-	      if (!(q[0]==0.0f && q[1]==0.0f && q[2]==0.0f && q[3]==0.0f))
+	      if (!BNO080_dataAvailable())
 	      {
-	    	  	  printf("Giroscopio: %.2f %.2f %.2f %.2f\r\n",
-	               q[0], q[1], q[2], q[3]);
-
-	        printf("Aceleracao: %.2f %.2f %.2f\r\n",
-	               BNO080_getAccelX(),
-	               BNO080_getAccelY(),
-	               BNO080_getAccelZ());
+			  q[0] = BNO080_getQuatI();
+			  q[1] = BNO080_getQuatJ();
+			  q[2] = BNO080_getQuatK();
+			  q[3] = BNO080_getQuatReal();
+			  BME280Calculation(&BME280);
+			  printf("Temperatura%f\n", BME280.Temperature);
+			  printf("Pressão%f\n", BME280.Pressure);
+			  printf("Altitude%f\n", BME280.AltitudeTP);
+			  printf("%f\n", BME280.Humidity);
+			  printf("Giroscopio: %.2f %.2f %.2f %.2f\r\n",
+			   q[0], q[1], q[2], q[3]);
+			  printf("Aceleracao: %.2f %.2f %.2f\r\n",
+					   BNO080_getAccelX(),
+					   BNO080_getAccelY(),
+					   BNO080_getAccelZ());
+			  printf("Magnetometro: %.2f %.2f %.2f\r\n",
+					  BNO080_getMagX(),
+					  BNO080_getMagY(),
+					  BNO080_getMagZ());
 	      }
 	      else
 	      {
 	        printf("quat all zero — check sensor / comms\r\n");
 	      }
-	    }
+
 	    HAL_Delay(50);
     /* USER CODE END WHILE */
 
@@ -158,11 +168,6 @@ int main(void)
   }
   /* USER CODE END 3 */
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void Sensor_Init(void)
 {
 
@@ -184,6 +189,11 @@ void Sensor_Init(void)
 
 	BME280Init(BME280_InitStruct);
 }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -260,6 +270,55 @@ static void MX_I2C1_Init(void)
   * @param None
   * @retval None
   */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  LL_SPI_InitTypeDef SPI_InitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_SPI2);
+
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+  /**SPI2 GPIO Configuration
+  PB10   ------> SPI2_SCK
+  PB14   ------> SPI2_MISO
+  PB15   ------> SPI2_MOSI
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_10|LL_GPIO_PIN_14|LL_GPIO_PIN_15;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_5;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  SPI_InitStruct.TransferDirection = LL_SPI_FULL_DUPLEX;
+  SPI_InitStruct.Mode = LL_SPI_MODE_MASTER;
+  SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_8BIT;
+  SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_HIGH;
+  SPI_InitStruct.ClockPhase = LL_SPI_PHASE_2EDGE;
+  SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
+  SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV16;
+  SPI_InitStruct.BitOrder = LL_SPI_MSB_FIRST;
+  SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
+  SPI_InitStruct.CRCPoly = 10;
+  LL_SPI_Init(SPI2, &SPI_InitStruct);
+  LL_SPI_SetStandard(SPI2, LL_SPI_PROTOCOL_MOTOROLA);
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
 
 /**
   * @brief USART2 Initialization Function
@@ -317,10 +376,74 @@ static void MX_USART2_UART_Init(void)
   * @param None
   * @retval None
   */
+static void MX_GPIO_Init(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOH);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_12|LL_GPIO_PIN_13);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_12|LL_GPIO_PIN_13;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
 
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -333,7 +456,8 @@ void Error_Handler(void)
 
   /* USER CODE END Error_Handler_Debug */
 }
-#ifdef USE_FULL_ASSERT
+
+#ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
